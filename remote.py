8d168427133fc646c0576b132f65df80cca9364a
Markus Engsner <markus.engsner@gmail.com>
2020-05-05 21:49:02 +0200

    Make remote importable
    Change: wrap the plotting part in main()

--- remote.py
+++ remote.py
@@ -1,11 +1,10 @@
 import pygame
-from matplotlib import pyplot as plt
-import matplotlib.animation as animation
+
 
 class remoteController:
     def __init__(self):
         print("Controller init.")
-        self.screen = pygame.display.set_mode([10,10]) #Piece of shit nightmare thing
+        self.screen = pygame.display.set_mode([10, 10])  # Piece of shit nightmare thing
         pygame.joystick.init()
         self.joy = pygame.joystick.Joystick(0)
         self.joy.init()
@@ -16,21 +15,27 @@ class remoteController:
     def getAxis(self, axis):
         return round(self.joy.get_axis(axis), 2)
 
-remote = remoteController()
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+def main():
+    from matplotlib import pyplot as plt
+    import matplotlib.animation as animation
+    def xyPlot(i, axis1, axis2):
+        pygame.event.get()
+        remote.update()
+        sx = remote.getAxis(axis1)
+        sy = -remote.getAxis(axis2)
+        print("SX:", sx)
+        print("SY:", sy)
+        ax.clear()
+        plt.axis([-1, 1, -1, 1])
+        ax.scatter(sx, sy)
 
-def xyPlot(i, axis1, axis2):
-    pygame.event.get()
-    remote.update()
-    sx = remote.getAxis(axis1)
-    sy = -remote.getAxis(axis2)
-    print("SX:", sx)
-    print("SY:", sy)
-    ax.clear()
-    plt.axis([-1, 1, -1, 1])
-    ax.scatter(sx, sy)
+    remote = remoteController()
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ani = animation.FuncAnimation(fig, xyPlot, fargs=(0, 1, remote), interval=50)
+    plt.show()
 
-ani = animation.FuncAnimation(fig, xyPlot, fargs=(0,1), interval=50)
-plt.show()
+
+if __name__ == '__main__':
+    main()

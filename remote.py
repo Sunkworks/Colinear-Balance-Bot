7435df20608b1851ce281c8110ad9beb118245a4
Markus Engsner <markus.engsner@gmail.com>
2020-05-05 21:55:58 +0200

    remove superflous code; formatting and renaming

--- remote.py
+++ remote.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-class remoteController:
+class RemoteController:
     def __init__(self):
         print("Controller init.")
         self.screen = pygame.display.set_mode([10, 10])  # Piece of shit nightmare thing
@@ -9,28 +9,25 @@ class remoteController:
         self.joy = pygame.joystick.Joystick(0)
         self.joy.init()
 
-    def update(self):
-        pygame.event.get()
-
-    def getAxis(self, axis):
+    def get_axis(self, axis):
         return round(self.joy.get_axis(axis), 2)
 
 
 def main():
     from matplotlib import pyplot as plt
     import matplotlib.animation as animation
+
     def xyPlot(i, axis1, axis2):
         pygame.event.get()
-        remote.update()
-        sx = remote.getAxis(axis1)
-        sy = -remote.getAxis(axis2)
+        sx = remote.get_axis(axis1)
+        sy = -remote.get_axis(axis2)
         print("SX:", sx)
         print("SY:", sy)
         ax.clear()
         plt.axis([-1, 1, -1, 1])
         ax.scatter(sx, sy)
 
-    remote = remoteController()
+    remote = RemoteController()
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ani = animation.FuncAnimation(fig, xyPlot, fargs=(0, 1, remote), interval=50)

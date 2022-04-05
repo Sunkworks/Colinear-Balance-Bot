af29fdef10ac94cb81bf586e6f2eb151060e78b2
Markus Engsner <markus.engsner@gmail.com>
2022-04-05 08:30:39 +0100

    rm ds4drv

--- rob_manager.sh
+++ rob_manager.sh
@@ -1,6 +1,6 @@
 cd /home/pi/Colinear-Balance-Bot-Test/
 
 sudo python3 -c "from indicator import *; indicate(0, (0, 255, 0))"
-sudo ds4drv >> ds4drv.log 2>&1 &
+#sudo ds4drv >> ds4drv.log 2>&1 &
 sudo python3 main.py
 

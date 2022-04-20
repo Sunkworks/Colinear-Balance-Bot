3fbaf14fa001b24d319cce94f01e79a248ae6277
inspirers <arvid.boisen@protonmail.com>
2022-04-20 13:13:08 +0200

    Indicator.py not in use

--- indicator.py
+++ indicator.py
@@ -1,8 +1,8 @@
 import neopixel
 import board
-
+# File is not in use anymore
 pixel_pin = board.D18 #File used for indicating LED-lights on robot.
-pixel_count = 3
+pixel_count = 3 
 pixel_order = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
